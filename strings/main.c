c380f799a9b07dca4853e4f4373458a81d3584ad
Oleg Andrianov <oleg.andrianov@bk.ru>
2024-04-06 17:21:45 +0300

    useless func

--- strings/main.c
+++ strings/main.c
@@ -4,22 +4,17 @@
 
 #define string char*
 
-int letters_counter(const string str);
 int vowels_counter(const string str);
 
 int main(void) {
     string example = "this is string";
-    int total_letters = letters_counter(example);
+    int total_letters = strlen(example);
     int total_vowels = vowels_counter(example);
 
     printf("total letters: %d\n", total_letters);
     printf("total vowels: %d\n", total_vowels);
 
     return 0;
-}
-
-int letters_counter(const string str) {
-    return strlen(str);
 }
 
 int vowels_counter(const string str) {
