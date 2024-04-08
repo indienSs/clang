a58e108eb7a4eb4230ad6d462edee55295267e6c
Oleg Andrianov <oleg.andrianov@bk.ru>
2024-04-08 11:57:21 +0300

    strings struct test

--- strings/main.c
+++ strings/main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
 #define string char*
 
 int vowels_counter(const string str);
+
+typedef struct {
+    int length;
+    char *value;
+} String;
+
+void* new_string(const string);
 
 int main(void) {
     string example = "this is string";
@@ -13,6 +21,11 @@ int main(void) {
 
     printf("total letters: %d\n", total_letters);
     printf("total vowels: %d\n", total_vowels);
+
+    String *strnew = (String *)new_string("this is new struct string");
+    printf("string struct value: %s\n", strnew->value);
+
+    free(strnew);
 
     return 0;
 }
@@ -30,4 +43,24 @@ int vowels_counter(const string str) {
         }
     }
     return counter;
+}
+
+void to_upper_case(String *str) {
+    for (int i = 0; i < str->length; i++) {
+        str->value[i] = toupper(str->value[i]);
+    }
+}
+
+void to_lower_case(String *str) {
+    for (int i = 0; i < str->length; i++) {
+        str->value[i] = tolower(str->value[i]);
+    }
+}
+
+void* new_string(const string str) {
+    String *strct = malloc(sizeof(int) + strlen(str));
+    strcpy_s(strct->value, strlen(str), str);
+    strct->length = strlen(str);
+
+    return (void*)strct;
 }
