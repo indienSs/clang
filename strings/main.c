d98813ec064c2c5351441ca3d6b78f8065c86dec
Oleg Andrianov <oleg.andrianov@bk.ru>
2024-05-01 15:11:33 +0300

    string: includes

--- strings/main.c
+++ strings/main.c
@@ -2,17 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 #define string char*
 
 int vowels_counter(const string str);
-
-typedef struct {
-    int length;
-    char *value;
-} String;
-
-void* new_string(const string);
+bool string_includes(char* str, char* word);
 
 int main(void) {
     string example = "this is string";
@@ -21,11 +16,6 @@ int main(void) {
 
     printf("total letters: %d\n", total_letters);
     printf("total vowels: %d\n", total_vowels);
-
-    String *strnew = (String *)new_string("this is new struct string");
-    printf("string struct value: %s\n", strnew->value);
-
-    free(strnew);
 
     return 0;
 }
@@ -45,22 +35,25 @@ int vowels_counter(const string str) {
     return counter;
 }
 
-void to_upper_case(String *str) {
-    for (int i = 0; i < str->length; i++) {
-        str->value[i] = toupper(str->value[i]);
+bool string_includes(char* str, char* word) {
+    int str_len = strlen(str);
+    int word_len = strlen(word);
+    int str_end = str_len - word_len + 1;
+    bool includes = false;
+
+    for (int i = 0; i < str_end; i++) {
+        bool found = true;
+        for (int j = 0; j < word_len; j++) {
+            if (word[j] != str[i + j]) {
+                found = false;
+                break;
+            }
+        }
+        if (found) {
+            includes = true;
+            break;
+        }
     }
-}
 
-void to_lower_case(String *str) {
-    for (int i = 0; i < str->length; i++) {
-        str->value[i] = tolower(str->value[i]);
-    }
-}
-
-void* new_string(const string str) {
-    String *strct = malloc(sizeof(int) + strlen(str));
-    strcpy_s(strct->value, strlen(str), str);
-    strct->length = strlen(str);
-
-    return (void*)strct;
+    return includes;
 }
