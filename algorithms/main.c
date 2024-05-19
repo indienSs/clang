533b88c6f4d659e844669023fbe62a22683f5e23
Oleg Andrianov <oleg.andrianov@bk.ru>
2024-05-19 17:14:16 +0300

    algorithm: bubble sort optimization

--- algorithms/main.c
+++ algorithms/main.c
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+
 void algorithhm_bubble_sort(int*, int);
 int calculate_fib(int);
 
@@ -6,15 +8,28 @@ int main(void) {
 }
 
 void algorithhm_bubble_sort(int* arr, int length) {
-    for (int i = 0; i < length; ++i) {
-        for (int j = 0; j < length - 1; ++j) {
-            if (arr[j] > arr[j+1]) {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+    // for (int i = 0; i < length; ++i) {
+    //     for (int j = 0; j < length - 1; ++j) {
+    //         if (arr[j] > arr[j+1]) {
+    //             int temp = arr[j];
+    //             arr[j] = arr[j+1];
+    //             arr[j+1] = temp;
+    //         }
+    //     }
+    // }
+
+    bool swapped = false;
+    do {
+        swapped = false;
+        for (int i = 0; i < length - 1; ++i) {
+            if (arr[i] > arr[i+1]) {
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+                swapped = true;
             }
         }
-    }
+    } while (swapped);
 }
 
 int algorithm_fibonacci(int num) {
