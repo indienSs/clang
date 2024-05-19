4dfb256f8452609b719fa1271d09afae8d6a5e80
Oleg Andrianov <oleg.andrianov@bk.ru>
2024-05-19 17:08:40 +0300

    algorithm: fibbonachi

--- algorithms/main.c
+++ algorithms/main.c
@@ -1,5 +1,5 @@
-
 void algorithhm_bubble_sort(int*, int);
+int calculate_fib(int);
 
 int main(void) {
     return 0;
@@ -15,4 +15,20 @@ void algorithhm_bubble_sort(int* arr, int length) {
             }
         }
     }
+}
+
+int algorithm_fibonacci(int num) {
+    if (num <= 1) return 0;
+    if (num == 2) return 1;
+    
+    int first = 0;
+    int second = 1;
+
+    for (int i = 0; i < num - 2; i++) {
+        int tmp = second;
+        second += first;
+        first = tmp;
+    }
+
+    return second;
 }
