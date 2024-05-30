0e5e79b662ad1859801abe436095c59ec9b1b9f5
Oleg Andrianov <oleg.andrianov@bk.ru>
2024-05-30 21:26:20 +0300

    files: read_file

---
+++ files/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char* files_read_file(char*);
+
+int main(void) {
+    return 0;
+}
+
+char* files_read_file(char* path) {
+    FILE *fb;
+    int buffer_size = 256;
+    char *buffer = malloc(sizeof(char) * buffer_size);
+
+    fb = fopen(path, "r");
+    if (fb == NULL) return "";
+    int i = 0;
+    while ((buffer[i] = fgetc(fb)) != EOF) {
+        i++;
+        if (buffer_size - 1 == i) {
+            buffer_size *= 2;
+            buffer = realloc(buffer, sizeof(char) * buffer_size);
+        }
+    }
+    return buffer;
+}
